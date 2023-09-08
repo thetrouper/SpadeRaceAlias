16696db346607c36a40e78a12dee29a844b5b974
trouper <crvn152006@gmail.com>
2023-09-08 05:44:58 -0500

    Trying to bypass max velocity

--- src/main/java/io/github/thetrouper/spaderacealias/events/JumpPadEvents.java
+++ src/main/java/io/github/thetrouper/spaderacealias/events/JumpPadEvents.java
@@ -1,22 +1,45 @@
 package io.github.thetrouper.spaderacealias.events;
 
+import io.github.thetrouper.spaderacealias.SpadeRaceAlias;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.util.Vector;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class JumpPadEvents {
 
     public static void useLimePad(Player p) {
-        Vector vec = new Vector(0,1.33,0);
+        Vector vec = new Vector(0,1.2,0);
         p.setVelocity(vec);
     }
     public static void useRedPad(Player p) {
-        Location playerLocation = p.getLocation();
-        Vector playerDirection = playerLocation.getDirection();
-        playerDirection.normalize();
-        Vector vec = playerDirection.multiply(1);
-        Vector up = new Vector(0,1.33,0);
-        Vector vecUp = vec.add(up);
-        p.setVelocity(vecUp);
+        Vector vec = addFlatVelocity(p, 1.5);
+        p.setVelocity(addUpVelocity(vec,0.9));
+    }
+
+    public static void useOrangePad(Player p) {
+        AtomicInteger counter = new AtomicInteger(0);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(SpadeRaceAlias.getInstance(),() -> {
+            if (counter.getAndDecrement() > 2) {
+                Vector vec = addFlatVelocity(p,2);
+                p.setVelocity(addUpVelocity(vec,1D));
+            }
+        },0,1);
+    }
+    public static Vector addFlatVelocity(Player p, double velocity) {
+        double yaw = Math.toRadians(p.getLocation().getYaw());
+
+        double xVelocity = -Math.sin(yaw) * velocity;
+        double zVelocity = Math.cos(yaw) * velocity;
+
+        return new Vector(xVelocity, p.getVelocity().getY(), zVelocity);
+    }
+
+    public static Vector addUpVelocity(Vector original, Double add) {
+        Vector up = new Vector(0,add,0);
+        return original.add(up);
     }
 }
--- src/main/java/io/github/thetrouper/spaderacealias/events/listeners/JumpPadListener.java
+++ src/main/java/io/github/thetrouper/spaderacealias/events/listeners/JumpPadListener.java
@@ -4,6 +4,7 @@ import io.github.thetrouper.spaderacealias.events.CustomListener;
 import io.github.thetrouper.spaderacealias.events.JumpPadEvents;
 import io.github.thetrouper.spaderacealias.utils.Cooldown;
 import io.github.thetrouper.spaderacealias.utils.Text;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -11,22 +12,29 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class JumpPadListener implements CustomListener {
-    public static final Cooldown<Player> cooldown = new Cooldown<>();
     @EventHandler
-    public void onMove(PlayerMoveEvent e) {
+    public void onDoubleJump(PlayerMoveEvent e) {
         Player p = e.getPlayer();
         Block standingOn = p.getLocation().clone().add(0,-1,0).getBlock();
-        if (standingOn.getType().equals(Material.LIME_CONCRETE)) {
-            if (!cooldown.isOnCooldown(p)) {
-                JumpPadEvents.useLimePad(p);
-                cooldown.addCooldown(p,1000);
-            }
+        Block blockBelow = p.getLocation().clone().add(0,-2,0).getBlock();
+        if (standingOn.getType().equals(Material.RED_CONCRETE)
+            || standingOn.getType().equals(Material.ORANGE_CONCRETE)
+            || standingOn.getType().equals(Material.LIME_CONCRETE)) {
+            p.setAllowFlight(true);
         }
-        if (standingOn.getType().equals(Material.RED_CONCRETE)) {
-            if (!cooldown.isOnCooldown(p)) {
+        if (p.isFlying() && p.getGameMode().equals(GameMode.ADVENTURE)) {
+            p.setFlying(false);
+            p.setAllowFlight(false);
+            if (blockBelow.getType().equals(Material.RED_CONCRETE)) {
                 JumpPadEvents.useRedPad(p);
-                cooldown.addCooldown(p,1000);
             }
+            if (blockBelow.getType().equals(Material.LIME_CONCRETE)) {
+                JumpPadEvents.useLimePad(p);
+            }
+            if (blockBelow.getType().equals(Material.ORANGE_CONCRETE)) {
+                JumpPadEvents.useOrangePad(p);
+            }
+
         }
     }
 }
